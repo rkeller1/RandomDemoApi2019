b77ae508d8b581f490ae5cd4ee5f4dce7f3a06f1
WhoCode <robertkeller04@gmail.com>
2019-12-05 15:37:05 +0100

    New

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ var _data = require("./_data");
 
 const PORT = process.env.PORT || 5000
 
+app.get("/", (req, res, next) => {
+	res.send("Welcome to CVN Fake Users API 2019!");
+  });
+
 app.use("/static", express.static(__dirname + "/public"));
 
 app.get("/users", (req, res, next) => {
   let fakeUsers = _data.getFakeUsers();
   res.json(fakeUsers);
 });
-app.get("/", (req, res, next) => {
-  res.send("Welcome to CVN Fake Users API!");
-});
 
 app.get("/products", (req, res, next) => {
   let getExplicit = req.query.explicit == 1;
-  console.log(getExplicit);
   let fakeProducts = _data.getFakeProducts(getExplicit);
   res.json(fakeProducts);
 });
